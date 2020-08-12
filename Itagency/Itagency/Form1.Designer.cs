6ef879ca209e03df740f24152265992708defec5
ahmed126742 <64670413+ahmed126742@users.noreply.github.com>
2020-08-12 06:56:40 -0700

    add

---
+++ Itagency/Itagency/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Itagency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.DepName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Salary = new System.Windows.Forms.TextBox();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "age";
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(151, 30);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(121, 20);
+            this.Name.TabIndex = 4;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(151, 69);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(121, 20);
+            this.age.TabIndex = 5;
+            // 
+            // DepName
+            // 
+            this.DepName.Location = new System.Drawing.Point(453, 26);
+            this.DepName.Name = "DepName";
+            this.DepName.Size = new System.Drawing.Size(100, 20);
+            this.DepName.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(337, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Department Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(68, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(489, 135);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Add Employee";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Add Department";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(572, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Display";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(572, 277);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "BackUp";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Salary";
+            // 
+            // Salary
+            // 
+            this.Salary.Location = new System.Drawing.Point(151, 99);
+            this.Salary.Name = "Salary";
+            this.Salary.Size = new System.Drawing.Size(121, 20);
+            this.Salary.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(65, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Depart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(873, 358);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Salary);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DepName);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+           // this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DepName;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Salary;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Itagency/Itagency/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Windows.Documents;
+using System.Data.Entity.Core.Common.EntitySql;
+using System.IO;
+
+namespace Itagency
+{
+    public partial class Form1 : Form
+    {
+
+        Employee emp = new Employee();
+        Department dep = new Department();
+        CompanyContxtEntities db = new CompanyContxtEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+            fillComboBOx();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            emp.Name = Name.Text;
+            emp.age = int.Parse(age.Text);
+            emp.Salary = Decimal.Parse(Salary.Text);
+            emp.departmentId = (int)comboBox1.SelectedIndex;
+            db.Employees.Add(emp);
+            db.SaveChanges();
+            Name.Text = "";
+            age.Text ="";
+            Salary.Text="";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dep.Name = DepName.Text;
+            db.Departments.Add(dep);
+            db.SaveChanges();
+            DepName.Text = "";
+            fillComboBOx();
+
+
+        }
+
+        private void  fillComboBOx()
+        {
+            //comboBox1.Items.Clear();
+            var data = db.Departments.ToList();
+            DataTable table = new DataTable();
+            table.Columns.Add("id");
+            table.Columns.Add("DepName");
+            foreach (var item in data)
+            {
+                table.Rows.Add(item.departmentId, item.Name);
+            }
+            comboBox1.DataSource = table;
+            comboBox1.DisplayMember = "DepName";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees
+                .Join(db.Departments,
+                      p => p.departmentId,
+                      c => c.departmentId,
+                      (p, c) => new {
+                          Name = p.Name,
+                          age = p.age,
+                          Salary = p.Salary,
+                          Department = c.Name
+                      }
+                      ).ToArray();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StringBuilder CsvFile = new StringBuilder();
+            CsvFile.AppendLine("Name , Age , Salary ,Department");
+            var _Data = db.Employees
+                .Join(db.Departments,
+                      p => p.departmentId,
+                      c => c.departmentId,
+                      (p, c) => new {
+                          Name = p.Name,
+                          age = p.age,
+                          Salary = p.Salary,
+                          Department = c.Name
+                      }
+                      ).ToArray();
+            foreach (var item in _Data)
+            {
+                CsvFile.AppendLine(item.Name+","+item.age+","+item.Salary+","+item.Department);
+            }
+            string Path = "E:\\DataBackup.csv"; // StATIC nName just right now 
+            File.AppendAllText(Path, CsvFile.ToString());
+        }
+    }
+}
